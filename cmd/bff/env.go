da2a620ac2776f05d8e58c69039e3c6f1228f476
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-01-09 16:04:01 +0900

    refactor: remove cmd/bff

--- cmd/bff/env.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type environment struct {
-	Envrionment       string `env:"ENV" envDefault:"development"`
-	Port              string `env:"PORT" envDefault:"8081"`
-	AuthenticationURL string `env:"PORT" envDefault:"localhost:50051"`
-}
